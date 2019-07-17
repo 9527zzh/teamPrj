9867534130d808c5cff8f13ab7b58266c84ea8f4
fx <4396@666.com>
2019-07-17 19:35:06 +0800

    范鑫的service

---
+++ src/main/java/com/woniu/service/impl/TreeServiceImpl.java
@@ -0,0 +1,41 @@
+package com.woniu.service.impl;
+
+import java.util.List;
+
+import com.woniu.model.Tree;
+import com.woniu.service.ITreeService;
+
+public class TreeServiceImpl implements ITreeService {
+
+	@Override
+	public void add(Tree tree) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Integer tid) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Tree tree) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Tree findOne(Integer tid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Tree> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
---
+++ src/main/java/com/woniu/service/impl/UserroleServiceImpl.java
@@ -0,0 +1,41 @@
+package com.woniu.service.impl;
+
+import java.util.List;
+
+import com.woniu.model.UserroleKey;
+import com.woniu.service.IUserroleService;
+
+public class UserroleServiceImpl implements IUserroleService {
+
+	@Override
+	public void add(UserroleKey userroleKey) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(UserroleKey userroleKey) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(UserroleKey userroleKey) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public UserroleKey findOne(UserroleKey userroleKey) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<UserroleKey> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
